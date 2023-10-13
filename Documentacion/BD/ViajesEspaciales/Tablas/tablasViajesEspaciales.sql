e80585b38ecf275afb0bb1368f6bf834ee0c525a
Santiago Rojas <santirojastr12@gmail.com>
2023-10-12 21:48:52 -0500

    Scripts para la creaci+on de tablas en la BD de ViajesEspaciales

---
+++ Documentacion/BD/ViajesEspaciales/Tablas/tablasViajesEspaciales.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE ViajesEspaciales
+
+USE ViajesEspaciales
+
+CREATE TABLE Profesion
+(
+	Profesion_Id INT PRIMARY KEY IDENTITY(1,1)
+	,Profesion_Nombre VARCHAR(60) NOT NULL
+)
+
+CREATE TABLE Astronauta
+(
+	Astronauta_Id INT PRIMARY KEY IDENTITY(1,1)
+	,Astronauta_Nombre VARCHAR(150) NOT NULL
+	,Astronauta_ProfesionId INT NOT NULL
+
+	,FOREIGN KEY (Astronauta_ProfesionId)  REFERENCES Profesion (Profesion_Id)
+)
+
+CREATE TABLE Objetivo
+(
+	Objetivo_Id INT PRIMARY KEY IDENTITY(1,1)
+	,Objetivo_Nombre VARCHAR(150) NOT NULL
+)
+
+CREATE TABLE Mision
+(
+	Mision_Id INT PRIMARY KEY IDENTITY(1,1)
+	,Mision_Nombre VARCHAR(150) NOT NULL
+	,Mision_Fecha DATETIME NOT NULL
+	,Mision_AstronautaId INT NOT NULL
+	,Mision_ObjetivoId INT NOT NULL
+
+	,FOREIGN KEY (Mision_AstronautaId) REFERENCES Astronauta (Astronauta_Id)
+	,FOREIGN KEY (Mision_ObjetivoId) REFERENCES Objetivo (Objetivo_Id)
+)
