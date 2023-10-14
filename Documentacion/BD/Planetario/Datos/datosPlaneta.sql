736c0534c114221daec1d14bddb4cd6e6ebf30e0
MateoVillamizar <matiusvil234@gmail.com>
2023-10-14 12:54:41 -0500

    Query de datos del planeta

--- Documentacion/BD/Planetario/Datos/datosPlaneta.sql
+++ Documentacion/BD/Planetario/Datos/datosPlaneta.sql
@@ -7,4 +7,23 @@ VALUES
 	,('Marte',	227940000,	687.00,	24.08,	1.85)
 	,('Júpiter', 778340821,	4333.6, 13.07,	1.30)
 
-	
+INSERT INTO 
+	Satelite(Satelite_Nombre, Satelite_Excentricidad, Satelite_PeriodoOrbital, Satelite_InclinacionOrbital)
+VALUES
+	('Luna', 0.0549, 27.32, 5.15)
+	,('Io', 0.0041, 1.77, 0.04 )
+	,('Europa', 0.0094, 3.55, 0.47)
+	,('Ganimedes', 0.0013, 7.16, 0.20)
+	,('Calisto', 0.0074, 16.69, 0.28)
+
+INSERT INTO
+	Planetario(Planetario_Nombre)
+VALUES
+	('Planetario Wonder')
+
+
+INSERT INTO
+	PlanetarioSatelites(PlanetarioSatelites_PlanetarioId, PlanetarioSatelites_SateliteId)
+VALUES
+	(1,1)
+	,(1,3)
