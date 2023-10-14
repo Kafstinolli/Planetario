d8093fe1d65d76d3e5874307db6c44103f242eae
Andres Gomez <agomez47@uan.edu.co>
2023-10-14 12:18:13 -0500

    Datos planetas

---
+++ Documentacion/BD/Planetario/Datos/datosPlaneta.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+	Planeta(Planeta_Nombre, Planeta_DistanciaSol, Planeta_PeriodoOrbital, Planeta_VelocidadOrbital, Planeta_InclinacionOrbital)
+VALUES
+	('Mercurio',57910000, 87.97, 47.87,	7.00)
+	,('Venus',	108200000, 224.70,	35.02, 3.39)
+	,('Tierra',	149600000,	365.25,	29.78,	0.00)
+	,('Marte',	227940000,	687.00,	24.08,	1.85)
+	,('Júpiter', 778340821,	4333.6, 13.07,	1.30)
+
+	
