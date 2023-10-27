98940280f8de6c61252857137a0c9cd7eb6b8d12
Santiago Rojas <santirojastr12@gmail.com>
2023-10-27 17:18:47 -0500

    Edicion del retorno del metodo GetAllPlanetas() reemplazandolo por una lista de Planeta

--- Proyecto/Planetario/PDB.cs
+++ Proyecto/Planetario/PDB.cs
@@ -4,10 +4,10 @@ public class PDB : PlanetarioDB
 {
     public PDB(string server, string user, string password) : base(server, user, password) { }
 
-    public void GetAllPlanetas()
+    public List<Planeta> GetAllPlanetas()
     {
         Connect();
-
+        List<Planeta> planetas = new List<Planeta>();
         string query = "SELECT * FROM Planetas";
         SqlCommand command = new SqlCommand(query, _connection);
 
@@ -15,9 +15,10 @@ public class PDB : PlanetarioDB
 
         while (reader.Read())
         {
-
+            x
         }
-
         CloseConnection();
+
+        return planetas;
     }
 }
