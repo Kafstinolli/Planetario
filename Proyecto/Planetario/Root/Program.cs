72e66b252995cd55c7b30a10505369dcde7fa242
Santiago Rojas <santirojastr12@gmail.com>
2023-10-27 23:00:10 -0500

    Se modifica la logica para realizar una prubea enviando una solicitud al loadBalancer

--- Proyecto/Planetario/Root/Program.cs
+++ Proyecto/Planetario/Root/Program.cs
@@ -1,12 +1,28 @@
 ﻿using Planetario.Data;
-using Planetario.Models;
+using Planetario.LoadBalancing;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        PlanetarioServer planetarioServer = new PlanetarioServer("softiRedsLaptop", "sa", "isabella12");
+        LoadBalancer loadBalancer = new LoadBalancer();
+        Request request = new Request("SELECT Planeta_Id, Planeta_Nombre, Planeta_DistanciaSol, Planeta_PeriodoOrbital, Planeta_VelocidadOrbital, Planeta_InclinacionOrbital FROM Planeta", "SELECT", "planetario");
+        loadBalancer.prueba(request);
+        // Show(planetarioServer);
+    }
+}
 
-        Show(planetarioServer);
+public class Request
+{
+    public string Nombre { get; }
+    public string Tipo { get; }
+
+    public string Id { get; }
+
+    public Request(string nombre, string tipo, string id)
+    {
+        Nombre = nombre;
+        Tipo = tipo;
+        Id = id;
     }
 }
