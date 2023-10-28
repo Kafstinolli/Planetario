279cd918d0d2130d1f88e187a8fd88c161b7f25a
Santiago Rojas <santirojastr12@gmail.com>
2023-10-27 21:53:21 -0500

    Cambio en la estructura del main, prueba con el metodo para mostrar planetas en el 'servidor' de Planetas

--- Proyecto/Planetario/Root/Program.cs
+++ Proyecto/Planetario/Root/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PlanetarioServer planetarioServer = new PlanetarioServer("softiRedsLaptop", "sa", "isabella12");
+
+        Show(planetarioServer);
+    }
+
+    public static void Show(PlanetarioServer planetarioServer)
+    {
+        Console.Clear();
+
+        List<Planeta> planetas = planetarioServer.GetAllPlanetas();
+
+        foreach (Planeta planeta in planetas)
+        {
+            Console.WriteLine($"| {planeta.Nombre} | {planeta.DistanciaSol} | {planeta.PeriodoOrbital} | {planeta.VelocidadOrbital} | {planeta.InclinacionOrbital} |");
+        }
+    }
+}
