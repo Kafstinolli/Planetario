dbd32b1cb4c377d6179c37da8963f933ace51cf7
Andres Gomez <agomez47@uan.edu.co>
2023-11-04 10:26:41 -0500

    conexion de la base de datos Viajes Espaciales

--- Proyecto/Planetario/Data/ViajesEspaciales/ViajesEspacialesConnection.cs
+++ Proyecto/Planetario/Data/ViajesEspaciales/ViajesEspacialesConnection.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 
 namespace Planetario.Data.ViajesEspaciales
 {
@@ -12,6 +7,20 @@ namespace Planetario.Data.ViajesEspaciales
         private string _connectionString;
         protected SqlConnection _connection;
 
+        public ViajesEspacialesConnection(string server, string user, string password)
+        {
+            _connectionString = $"Data Source={server};Initial Catalog=ViajesEspaciales;User Id={user};Password={password}";
+        }
         
+        public void Connect()
+        {
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection != null && _connection.State == System.Data.ConnectionState.Open) _connection.Close();   
+        }
     }
 }
