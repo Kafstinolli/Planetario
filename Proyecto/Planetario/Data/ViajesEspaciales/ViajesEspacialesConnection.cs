90902c32acb2c4fe1948fb6b8b312ce399c9c1f1
Ximena Delgado <xim.delgado@outlook.com>
2023-11-04 10:06:43 -0500

    Creacion de base para conexion con Viajes Espaciales

--- Proyecto/Planetario/Data/ViajesEspaciales/ViajesEspacialesConnection.cs
+++ Proyecto/Planetario/Data/ViajesEspaciales/ViajesEspacialesConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace Planetario.Data.ViajesEspaciales
 {
     public class ViajesEspacialesConnection
     {
+        private string _connectionString;
+        protected SqlConnection _connection;
+
+        
     }
 }
