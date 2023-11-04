b1d993308f384052c8845f583477e23178120a31
Ximena Delgado <xim.delgado@outlook.com>
2023-11-04 10:03:15 -0500

    Cambio en el namespace

--- Proyecto/Planetario/Data/Planetario/PlanetarioServer.cs
+++ Proyecto/Planetario/Data/Planetario/PlanetarioServer.cs
@@ -1,7 +1,7 @@
 using System.Data.SqlClient;
 using Planetario.Models;
 
-namespace Planetario.Data;
+namespace Planetario.Data.Planetario;
 
 public class PlanetarioServer : PlanetarioConnection
 {
---
+++ Proyecto/Planetario/Data/ViajesEspaciales/ViajesEspacialesConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetario.Data.ViajesEspaciales
+{
+    public class ViajesEspacialesConnection
+    {
+    }
+}
