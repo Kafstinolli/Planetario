99c7bf689244276a2c729b38bc4e0fadf63b02a3
Santiago Rojas <santirojastr12@gmail.com>
2023-11-08 21:55:21 -0500

    Cambio de posicion de las conexiones y nuevo nombre para el metodo de SELECT

--- Proyecto/Planetario/LoadBalancing/LoadBalancer.cs
+++ Proyecto/Planetario/LoadBalancing/LoadBalancer.cs
@@ -11,47 +11,37 @@ public class LoadBalancer
 {
     private PlanetarioController _planetarioController = new PlanetarioController();
     private ViajesEspacialesController _viajesEspacialesController = new ViajesEspacialesController();
-    
-    public void prueba(Request request)
+
+    //  Conexi�n Santiago:
+    //  PlanetarioServer planetarioServer = new PlanetarioServer("softiRedsLaptop", "sa", "isabella12"); 
+    //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("softiRedsLaptop", "sa", "isabella12"); 
+
+    //  Conexi�n Mateo:
+    //  PlanetarioServer planetarioServer = new PlanetarioServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
+    //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
+
+    //  Conexi�n Juanes:
+    //  PlanetarioServer planetarioServer = new PlanetarioServer("JUANES", "sa", "juanes211520");
+    //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("JUANES", "sa", "juanes211520");
+
+    //  Conexi�n Ximena:
+    //  PlanetarioServer planetarioServer = new PlanetarioServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
+    //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
+    // :( 
+
+    //  Conexi�n Andr�s:
+    //  PlanetarioServer planetarioServer = new PlanetarioServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
+    //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
+
+
+    public void SELECT(Request request)
     {
         if (request.Id == "planetario")
         {
-            //  Conexi�n Santiago:
-            //  PlanetarioServer planetarioServer = new PlanetarioServer("softiRedsLaptop", "sa", "isabella12"); 
-
-            //  Conexi�n Mateo:
-            //  PlanetarioServer planetarioServer = new PlanetarioServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
-
-            //  Conexi�n Juanes:
-            //  PlanetarioServer planetarioServer = new PlanetarioServer("JUANES", "sa", "juanes211520");
-
-            //  Conexi�n Ximena:
-            //  PlanetarioServer planetarioServer = new PlanetarioServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
-            // :( 
-
-            //  Conexi�n Andr�s:
-            //  PlanetarioServer planetarioServer = new PlanetarioServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
-
             //  _planetarioController.Show(planetarioServer, request.Query);
         }
-        else if(request.Id == "viajesEspaciales")
+        else if (request.Id == "viajesEspaciales")
         {
-            //  Conexi�n Santiago:
-            //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("softiRedsLaptop", "sa", "isabella12"); 
-
-            //  Conexi�n Mateo:
-            //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
-
-            //  Conexi�n Juanes:
-            //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("JUANES", "sa", "juanes211520");
-
-            //  Conexi�n Ximena:
-            //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
-            // :( 
-
-            //  Conexi�n Andr�s:
-            //  ViajesEspacialesServer viajesEspacialesServer = new ViajesEspacialesServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
-
             //  _viajesEspacialesController.Show(viajesEspacialesServer, request.Query);
         }
     }
