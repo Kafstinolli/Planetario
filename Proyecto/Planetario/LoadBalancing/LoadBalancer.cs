0566e398e7c2061dbddc1860047c394f3e703e05
Santiago Rojas <santirojastr12@gmail.com>
2023-11-08 22:26:17 -0500

    Cambios en las posiciones de las inicializaciones de objetos de servidores

--- Proyecto/Planetario/LoadBalancing/LoadBalancer.cs
+++ Proyecto/Planetario/LoadBalancing/LoadBalancer.cs
@@ -15,30 +15,29 @@ public class LoadBalancer
     private PlanetarioServer _planetarioServer;
     private ViajesEspacialesServer _viajesEspacialesServer;
 
-    //  Conexi�n Santiago:
-    //   _planetarioServer = new PlanetarioServer("softiRedsLaptop", "sa", "isabella12"); 
-    //   _viajesEspacialesServer = new ViajesEspacialesServer("softiRedsLaptop", "sa", "isabella12"); 
-
-    //  Conexi�n Mateo:
-    //   _planetarioServer = new PlanetarioServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
-    //   _viajesEspacialesServer = new ViajesEspacialesServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
-
-    //  Conexi�n Juanes:
-    //   _planetarioServer = new PlanetarioServer("JUANES", "sa", "juanes211520");
-    //   _viajesEspacialesServer = new ViajesEspacialesServer("JUANES", "sa", "juanes211520");
-
-    //  Conexi�n Ximena:
-    //   _planetarioServer = new PlanetarioServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
-    //   _viajesEspacialesServer = new ViajesEspacialesServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
-    // :( 
-
-    //  Conexi�n Andr�s:
-    //   _planetarioServer = new PlanetarioServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
-    //   _viajesEspacialesServer = new ViajesEspacialesServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
-
-
     public void SELECT(Request request)
     {
+        //  Conexi�n Santiago:
+        //   _planetarioServer = new PlanetarioServer("softiRedsLaptop", "sa", "isabella12");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("softiRedsLaptop", "sa", "isabella12"); 
+
+        //  Conexi�n Mateo:
+        //   _planetarioServer = new PlanetarioServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
+
+        //  Conexi�n Juanes:
+        //   _planetarioServer = new PlanetarioServer("JUANES", "sa", "juanes211520");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("JUANES", "sa", "juanes211520");
+
+        //  Conexi�n Ximena:
+        //   _planetarioServer = new PlanetarioServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
+        // :( 
+
+        //  Conexi�n Andr�s:
+        //   _planetarioServer = new PlanetarioServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
+
         if (request.Id == "planetario")
         {
             _planetarioController.Show(_planetarioServer, request.Query);
@@ -51,6 +50,27 @@ public class LoadBalancer
 
     public void INSERT(Request request)
     {
+        //  Conexi�n Santiago:
+        //   _planetarioServer = new PlanetarioServer("softiRedsLaptop", "sa", "isabella12");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("softiRedsLaptop", "sa", "isabella12"); 
+
+        //  Conexi�n Mateo:
+        //   _planetarioServer = new PlanetarioServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("DESKTOP-RGNT8FG\\SQLEXPRESS", "sa", "Project1");
+
+        //  Conexi�n Juanes:
+        //   _planetarioServer = new PlanetarioServer("JUANES", "sa", "juanes211520");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("JUANES", "sa", "juanes211520");
+
+        //  Conexi�n Ximena:
+        //   _planetarioServer = new PlanetarioServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("LAPTOP-0G61ILIU\\SQLEXPRESS", "sa", "sql123");
+        // :( 
+
+        //  Conexi�n Andr�s:
+        //   _planetarioServer = new PlanetarioServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
+        //   _viajesEspacialesServer = new ViajesEspacialesServer("ANDRES\\SQLEXPRESS", "sa", "Kilian16");
+
         if (request.Id == "planetario")
         {
             _planetarioController.Add(_planetarioServer, request.Query);
--- Proyecto/Planetario/Root/Program.cs
+++ Proyecto/Planetario/Root/Program.cs
@@ -8,8 +8,10 @@ internal class Program
     {
         // LoadBalancer loadBalancer = new LoadBalancer();
         // Request request = new Request("SELECT Planeta_Id, Planeta_Nombre, Planeta_DistanciaSol, Planeta_PeriodoOrbital, Planeta_VelocidadOrbital, Planeta_InclinacionOrbital FROM Planeta", "SELECT", "planetario");
+        // Request request = new Request("INSERT INTO Planeta (Planeta_Nombre, Planeta_DistanciaSol, Planeta_PeriodoOrbital, Planeta_VelocidadOrbital, Planeta_InclinacionOrbital) VALUES ('PRUEBA', 1.0, 1.0, 1.0, 1.0)", "planetario");
         // Request request = new Request("SELECT Mision_Id, Mision_Nombre, Mision_Fecha, Mision_AstronautaId, Mision_ObjetivoId FROM Mision", "SELECT", "viajesEspaciales");
-        // loadBalancer.prueba(request);
+        // loadBalancer.SELECT(request);
+        // loadBalancer.INSERT(request);
 
         MenuPrincipal menu = new MenuPrincipal();
 
