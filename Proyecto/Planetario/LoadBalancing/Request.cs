7df08a06cfc1e7c9da8a59bb931fa3f94c35c0b4
Santiago Rojas <santirojastr12@gmail.com>
2023-11-08 21:56:27 -0500

    Eliminación de atributo Tipo en clase Request

--- Proyecto/Planetario/LoadBalancing/Request.cs
+++ Proyecto/Planetario/LoadBalancing/Request.cs
@@ -3,14 +3,11 @@ namespace Planetario.LoadBalancing;
 public class Request
 {
     public string Query { get; }
-    public string Tipo { get; }
-
     public string Id { get; }
 
-    public Request(string query, string tipo, string id)
+    public Request(string query, string id)
     {
         Query = query;
-        Tipo = tipo;
         Id = id;
     }
 }
