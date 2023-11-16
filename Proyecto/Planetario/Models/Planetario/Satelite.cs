442484ba44657b29b187cee6262ed6b6f45aee4b
Santiago Rojas <santirojastr12@gmail.com>
2023-11-15 21:31:30 -0500

    Creacion modelo Satelite

---
+++ Proyecto/Planetario/Models/Planetario/Satelite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetario.Models.Planetario
+{
+    public class Satelite
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Excentricidad { get; set; }
+        public double PeriodoOrbital { get; set; }
+        public double InclinacionOrbital { get; set; }
+
+        public Satelite(int id, string nombre, double excentricidad, double periodoOrbital, double inclinacionOrbital)
+        {
+            Id = id;
+            Nombre = nombre;
+            Excentricidad = excentricidad;
+            PeriodoOrbital = periodoOrbital;
+            InclinacionOrbital = inclinacionOrbital;
+        }
+    }
+}
