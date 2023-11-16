324090872926fb77b5d8c401f405eab3afd75283
Santiago Rojas <santirojastr12@gmail.com>
2023-11-15 21:38:56 -0500

    Configuracion operaciones para satelite

--- Proyecto/Planetario/Data/Planetario/PlanetarioServer.cs
+++ Proyecto/Planetario/Data/Planetario/PlanetarioServer.cs
@@ -7,6 +7,7 @@ public class PlanetarioServer : PlanetarioConnection
 {
     public PlanetarioServer(string server, string user, string password) : base(server, user, password) { }
 
+    //  Operaciones Planeta
     public void CreatePlaneta(string query)
     {
         Connect();
@@ -54,6 +55,62 @@ public class PlanetarioServer : PlanetarioConnection
     }
 
     public void DeletePlaneta(string query)
+    {
+        Connect();
+
+        SqlCommand command = new SqlCommand(query, _connection);
+        command.ExecuteNonQuery();
+
+        CloseConnection();
+    }
+
+    //  Operaciones Satelite
+    public void CreateSatelite(string query)
+    {
+        Connect();
+
+        SqlCommand command = new SqlCommand(query, _connection);
+        command.ExecuteNonQuery();
+
+        CloseConnection();
+    }
+
+    public List<Satelite> ReadSatelites(string query)
+    {
+        Connect();
+
+        List<Satelite> satelites = new List<Satelite>();
+        SqlCommand command = new SqlCommand(query, _connection);
+
+        SqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            string nombre = reader.GetString(1);
+            double excentricidad = reader.GetDouble(2);
+            double periodoOrbital = reader.GetDouble(3);
+            double inclinacionOrbital = reader.GetDouble(5);
+
+            satelites.Add(new Satelite(id, nombre, excentricidad, periodoOrbital, inclinacionOrbital));
+        }
+
+        CloseConnection();
+
+        return satelites;
+    }
+
+    public void UpdateSatelite(string query)
+    {
+        Connect();
+
+        SqlCommand command = new SqlCommand(query, _connection);
+        command.ExecuteNonQuery();
+
+        CloseConnection();
+    }
+
+    public void DeleteSatelite(string query)
     {
         Connect();
 
