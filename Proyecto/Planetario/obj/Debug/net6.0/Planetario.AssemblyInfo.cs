1617bbe568e9ad75d504ea43014cb0eb5120af2c
Santiago Rojas <santirojastr12@gmail.com>
2023-11-15 23:41:17 -0500

    Pruebas con la BD viajes espaciales utilizando el modelo Profesion

--- Proyecto/Planetario/obj/Debug/net6.0/Planetario.AssemblyInfo.cs
+++ Proyecto/Planetario/obj/Debug/net6.0/Planetario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Planetario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2a30d0f13b6809400fd3d4d0a4e5a378592588")]
 [assembly: System.Reflection.AssemblyProductAttribute("Planetario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Planetario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
