4e4bf2f74c53c7a551a61b1a586febe65822adfc
Santiago Rojas <santirojastr12@gmail.com>
2023-11-17 22:21:41 -0500

    maldita carpeta .vs

---
+++ Proyecto/Planetario/Backend/Planetario/PlanetarioController.cs
@@ -0,0 +1,57 @@
+﻿using NPlanetario.Data.DPlanetario;
+using NPlanetario.Models.MPlanetario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPlanetario.Backend.BPlanetario
+{
+    public class PlanetarioController
+    {
+        public void Add(PlanetarioServer planetarioServer, string query)
+        {
+            Console.Clear();
+
+            planetarioServer.CreatePlanetario(query);
+
+            Console.WriteLine("Se creó el planetario correctamente");
+        }
+
+        public void Show(PlanetarioServer planetarioServer, string query)
+        {
+            Console.Clear();
+
+            List<Planetario> planetarios = planetarioServer.ReadPlanetarios(query);
+
+            foreach (Planetario planetario in planetarios)
+            {
+                Console.WriteLine($"| {planetario.Id} | {planetario.Nombre} |");
+            }
+        }
+
+        public void Edit(PlanetarioServer planetarioServer, string query, int id)
+        {
+            Console.Clear();
+
+            query += $" WHERE Planetario_Id = {id}";
+
+            planetarioServer.UpdatePlanetario(query);
+
+            Console.WriteLine("Se editó el planetario correctamente");
+        }
+
+        public void Remove(PlanetarioServer planetarioServer, string query, int id)
+        {
+            Console.Clear();
+
+            query += $" WHERE Planetario_Id = {id}";
+
+            planetarioServer.DeletePlanetario(query);
+
+            Console.WriteLine("Se eliminó el planetario correctamente");
+        }
+    }
+
+}
---
+++ Proyecto/Planetario/Models/Planetario/Planetario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPlanetario.Models.MPlanetario
+{
+    public class Planetario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public Planetario(int id, string nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }
+    }
+}
