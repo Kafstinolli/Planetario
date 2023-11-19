11f29924ce6ec432e3ea07f3056eecf776e41e65
Santiago Rojas <santirojastr12@gmail.com>
2023-11-18 21:03:32 -0500

    Se configura el controlador para planetario satelites

---
+++ Proyecto/Planetario/Backend/Planetario/PlanetarioSateliteController.cs
@@ -0,0 +1,24 @@
+﻿using NPlanetario.Data.DPlanetario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetario.Backend.Planetario
+{
+    public class PlanetarioSateliteController
+    {
+        public void Add(PlanetarioServer planetarioServer, string query)
+        {
+            planetarioServer.CreatePlanetarioSatelites(query);
+        }
+
+        public void Remove(PlanetarioServer planetarioServer, string query, int id)
+        {
+            query += $" WHERE PlanetarioSatelites_Id = {id}";
+
+            planetarioServer.DeletePlanetarioSatelites(query);
+        }
+    }
+}
