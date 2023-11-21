8454bdf261a9747f39600aea14676a8caa24dd35
Santiago Rojas <santirojastr12@gmail.com>
2023-11-20 21:19:41 -0500

    Creacion de menu para crear astronauta

---
+++ Proyecto/Planetario/Frontend/Crear/Viajes Espaciales/CrearAstronauta.cs
@@ -0,0 +1,30 @@
+﻿using NPlanetario.LoadBalancing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetario.Frontend.Crear.Viajes_Espaciales
+{
+    public class CrearAstronauta
+    {
+        public static void Crear()
+        {
+            LoadBalancer loadBalancer = new LoadBalancer();
+
+            string nombreAstronauta;
+            int profesionId;
+
+            Console.WriteLine("Digite el nombre del astronauta:");
+            nombreAstronauta = Console.ReadLine();
+
+            Console.WriteLine("Digite el ID de la profesion del astronauta: ");
+            profesionId = Convert.ToInt32(Console.ReadLine());
+
+            Request request = new Request($"INSERT INTO Astronauta (Astronauta_Nombre, Astronauta_ProfesionId) VALUES ('{nombreAstronauta}', {profesionId})", "viajesEspaciales", "astronauta");
+            loadBalancer.INSERT(request);
+        }
+
+    }
+}
