2cc796eef1de51efc42baacad62cc50831d0f74f
Santiago Rojas <santirojastr12@gmail.com>
2023-11-22 01:10:15 -0500

    Creacion de menu para la relacion entre planetario y satelites

--- Proyecto/Planetario/Frontend/Principal/Planetario/MenuPlanetario.cs
+++ Proyecto/Planetario/Frontend/Principal/Planetario/MenuPlanetario.cs
@@ -5,6 +5,7 @@ using Planetario.Frontend.Crear.Planetario;
 using Planetario.Frontend.Editrar.Planetario;
 using Planetario.Frontend.Eliminar.Planetario;
 using Planetario.Frontend.Mostrar.Planetario;
+using Planetario.Frontend.Relaciones.Planetario;
 using System.Collections;
 using System.Diagnostics;
 
@@ -25,6 +26,8 @@ public class MenuPlanetario
             Console.WriteLine("2. Visualizar planetarios");
             Console.WriteLine("3. Editar planetario");
             Console.WriteLine("4. Eliminar planetario");
+            Console.WriteLine("5. Asignar satelite natural a planetario");
+            Console.WriteLine("6. Asignar planeta a planetario");
             Console.WriteLine("0. Salir del menu planetario");
             Console.WriteLine("Seleccione una opcion: ");
 
@@ -47,6 +50,13 @@ public class MenuPlanetario
 
                 case 4:
                     EliminarPlanetario.Eliminar();
+                    break;
+
+                case 5:
+                    PlanetarioSatelite.MostrarMenu();
+                    break;
+
+                case 6:
                     break;
 
                 case 0:
---
+++ Proyecto/Planetario/Frontend/Relaciones/Planetario/PlanetarioSatelite.cs
@@ -0,0 +1,34 @@
+﻿using NPlanetario.LoadBalancing;
+using Planetario.Frontend.Mostrar.Planetario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetario.Frontend.Relaciones.Planetario
+{
+    public class PlanetarioSatelite
+    {
+        public static void MostrarMenu()
+        {
+            Console.Clear();
+
+            LoadBalancer loadBalancer = new LoadBalancer();
+
+            MostrarPlanetarios.Mostrar();
+
+            Console.WriteLine("Digite el ID del planetario al que se le asignará un satelite:");
+            int idPlanetario = Convert.ToInt32(Console.ReadLine());
+
+            MostrarSatelites.Mostrar();
+
+            Console.WriteLine("Digite el ID del satelite que será asignado al planetario");
+            int idSatelite = Convert.ToInt32(Console.ReadLine());
+
+            Request request = new Request($"INSERT INTO PlanetarioSatelites (PlanetarioSatelites_PlanetarioId, PlanetarioSatelites_SateliteId) VALUES ({idPlanetario}, {idSatelite})", "planetario", "planetarioSatelite");
+
+            loadBalancer.INSERT(request);
+        }
+    }
+}
