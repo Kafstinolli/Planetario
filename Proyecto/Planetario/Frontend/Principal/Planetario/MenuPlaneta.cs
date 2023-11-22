2aa0b6262070d79a28a58af637de44761d551e6a
Santiago Rojas <santirojastr12@gmail.com>
2023-11-21 23:18:14 -0500

    Creacion de menu principal para Planeta

---
+++ Proyecto/Planetario/Frontend/Principal/Planetario/MenuPlaneta.cs
@@ -0,0 +1,58 @@
+﻿using Planetario.Frontend.Crear.Planetario;
+using Planetario.Frontend.Editrar.Planetario;
+using Planetario.Frontend.Eliminar.Planetario;
+using Planetario.Frontend.Mostrar.Planetario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planetario.Frontend.Principal.Planetario
+{
+    public class MenuPlaneta
+    {
+        public static void MostrarMenu()
+        {
+            int _opcionUsuario = 9;
+
+            Console.Clear();
+
+            while (_opcionUsuario != 0)
+            {
+                Console.WriteLine("\nMenu Planeta");
+                Console.WriteLine("1. Crear planeta");
+                Console.WriteLine("2. Visualizar planetas");
+                Console.WriteLine("3. Editar planeta");
+                Console.WriteLine("4. Eliminar planeta");
+                Console.WriteLine("0. Salir del menu planeta");
+                Console.WriteLine("Seleccione una opcion: ");
+
+                _opcionUsuario = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (_opcionUsuario)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        MostrarMenu();
+                        break;
+                }
+            }
+        }
+    }
+}
